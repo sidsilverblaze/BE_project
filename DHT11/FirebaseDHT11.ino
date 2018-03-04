a8c8da0d185f32bf345d8c326fac6be2acc16941
siddharth <siddharthpotty59@gmail.com>
2018-03-05 00:26:46 +0530

    Add files via upload

---
+++ DHT11/FirebaseDHT11.ino
@@ -0,0 +1,39 @@
+#include <FirebaseArduino.h>
+#include "DHT.h"
+
+#include  <ESP8266WiFi.h>
+
+#define FIREBASE_HOST "nodemcupir.firebaseio.com"
+#define FIREBASE_AUTH "1TheXTgjPcbxFv8Akmjmld6rcFm5l3yxbaiBIsnG"
+#define WIFI_SSID "TP-LINK_3880"
+#define WIFI_PASSWORD "sid123456"
+
+#define DHTPIN 0    // Data Pin of DHT 11 , for NodeMCU D5 GPIO no. is 14
+
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+
+   Serial.begin(9600) ;
+   WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
+   while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+   dht.begin();
+  Serial.println ("");
+  Serial.println ("WiFi Connected!");
+  Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
+  
+}
+
+void loop() {
+ 
+  float h = dht.readHumidity();
+  
+  float t = dht.readTemperature();  // Reading temperature as Celsius (the default)
+  Firebase.setFloat ("Temp",t);
+  Firebase.setFloat ("Humidity",h);
+  delay(200);
+}
