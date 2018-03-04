20bc7a89e225f0b6cdba2a019906cd9a223ef894
siddharth <siddharthpotty59@gmail.com>
2018-03-05 00:29:10 +0530

    Add files via upload

---
+++ water sensor/Firebasewatersensor.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal.h>
+#include <FirebaseArduino.h>
+
+#define FIREBASE_HOST "nodemcupir.firebaseio.com"
+#define FIREBASE_AUTH "1TheXTgjPcbxFv8Akmjmld6rcFm5l3yxbaiBIsnG"
+#define WIFI_SSID "TP-LINK_3880"
+#define WIFI_PASSWORD "sid123456"
+
+
+void setup(){
+  Serial.begin(9600);     // Communication started with 9600 baud
+  Serial.println ("WiFi Connected!");
+  Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
+  
+}
+void loop(){
+int sensor=analogRead(A0); // Incoming analog signal read and appointed sensor
+//Serial.println(sensor);   //Wrote serial port
+Firebase.setFloat ("Sensor",sensor);
+
+
+delay(1000);
+
+}
