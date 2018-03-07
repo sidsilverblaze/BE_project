5daea18e51a7c2a3726ebef039c387a89103ace8
siddharth <siddharthpotty59@gmail.com>
2018-03-07 16:16:20 +0530

    Add files via upload

---
+++ air sensor/Air_sensor.ino
@@ -0,0 +1,25 @@
+int input = A0;
+int output = 7;
+int th=500; // Set threshold level.
+void setup()
+{
+Serial.begin(9600);
+//pinMode(output,OUTPUT);
+//digitalWrite(output,LOW);
+
+}
+void loop()
+{
+Serial.println(analogRead(input));
+if(analogRead(input)>th)
+{
+//digitalWrite(output,HIGH);
+Serial.println("HIGH");
+}
+else
+{
+//digitalWrite(output,LOW);
+Serial.println("LOW");
+}
+delay(500);
+}
